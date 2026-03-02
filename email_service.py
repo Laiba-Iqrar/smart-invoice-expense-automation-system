6014c321a5a02a86360b12f2ab2aa415a1c0dc3f
Laiba-Iqrar <laibaiqrarahmedkhan@gmail.com>
2026-03-02 18:31:43 +0500

    change email functionality to only send invoice summary mail in batch

--- email_service.py
+++ email_service.py
@@ -9,7 +9,7 @@ SMTP_SERVER = "smtp.gmail.com"  # Change if not Gmail
 SMTP_PORT = 587
 
 SENDER_EMAIL = "zia4500438@cloud.neduet.edu.pk"
-SENDER_PASSWORD = ""  # ⚠️ Replace
+SENDER_PASSWORD = ""  #  Replace
 
 RECIPIENTS = [
     "laibaiqrarahmedkhan@gmail.com",
@@ -17,28 +17,37 @@ RECIPIENTS = [
 ]
 
 
-# =========================
-# SEND EMAIL FUNCTION
-# =========================
-def send_invoice_email(invoice):
+def send_summary_email(invoices):
 
-    subject = f"New Invoice Processed - {invoice['invoice_no']}"
+    if not invoices:
+        return  # Nothing to send
+
+    total_amount = sum(inv["total_amount"] for inv in invoices)
+    total_invoices = len(invoices)
+
+    subject = f"Invoice Summary Report ({total_invoices} invoices)"
 
     body = f"""
-Invoice Summary
+Invoice Summary Report
+==============================
 
-Invoice No: {invoice['invoice_no']}
-Vendor: {invoice['vendor']}
-Date: {invoice['date']}
-Total Amount: ${invoice['total_amount']:,.2f}
+Total Invoices: {total_invoices}
+Total Revenue: ${total_amount:,.2f}
 
-Items:
+--------------------------------
+Invoice Details:
+--------------------------------
 """
 
-    for item in invoice["items"]:
-        body += f"- {item['name']} | ${item['price']:,.2f} | {item['category']}\n"
+    for inv in invoices:
+        body += f"""
+Invoice No: {inv['invoice_no']}
+Vendor: {inv['vendor']}
+Date: {inv['date']}
+Total: ${inv['total_amount']:,.2f}
+--------------------------------
+"""
 
-    # Create message
     msg = MIMEMultipart()
     msg["From"] = SENDER_EMAIL
     msg["To"] = ", ".join(RECIPIENTS)
@@ -53,7 +62,7 @@ Items:
         server.sendmail(SENDER_EMAIL, RECIPIENTS, msg.as_string())
         server.quit()
 
-        print("Email notification sent successfully.")
+        print("Summary email sent successfully.")
 
     except Exception as e:
-        print("Failed to send email:", e)
+        print("Failed to send summary email:", e)
